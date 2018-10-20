9ab7b4408d7985b53a79e0b29248c6712d09232d
IceOny <emmanuelroic@gmail.com>
2018-10-20 12:06:51 +0100

    new api uri

--- js/script.js
+++ js/script.js
@@ -36,18 +36,79 @@ function getData() {
 getData();
 }
 */
-function  func(){
+/*function convertResponse(arr){
+  var i;
+  var resArr = [][];
+  var arrlen = arr.length;
+
+  for(i = 0; i < 5; i++){
+      resArr[i][0] = arr[i].ID;
+
+  }
+  for(i = 0; i < 5; i++){
+      resArr[i][1] = arr[i].Item;
+
+  }
+  for(i = 0; i < 5; i++){
+      resArr[i][2] = arr[i].Category;
+
+  }
+  for(i = 0; i < 5; i++){
+      resArr[i][3] = arr[i].Stock;
+
+  }
+  for(i = 0; i < 5; i++){
+      resArr[i][4] = arr[i].Price;
+
+  }
+  return resArr;
+}*/
+var func;
+func =  function () {
 var request = new XMLHttpRequest();
-//request.open('GET', 'http://localhost:3000/');
-request.open('GET', 'https://calm-beyond-30921.herokuapp.com');
-request.responseType = 'text';
+request.open('GET', 'https://store-manager-app-api.herokuapp.com/api/v1/products');
+//request.open('GET', 'https://calm-beyond-30921.herokuapp.com');
+//request.responseType = 'text';
 
 request.onload = function() {
-  document.getElementById('test').innerHTML = request.response;
+  var data = JSON.parse(request.response);
+  var dat = '';
+  var tf = document.getElementById('sr');
+  //document.getElementById('test').innerHTML = data[0].Item;
+  for (i = 0; i < data.length; i++) {
+    if(i==0){
+      dat += '<tr id="fs">';
+      dat += '<td>' + data[i].ID + '</td>';
+      dat += '<td>' + data[i].Item + '</td>';
+      dat += '<td>' + data[i].Category + '</td>';
+      dat += '<td>' + data[i].Stock + '</td>';
+      dat += '<td>' + data[i].Price + '</td>';
+      dat += '</tr>';
+    } else{
+      dat += '<tr>';
+   dat += '<td>' + data[i].ID + '</td>';
+   dat += '<td>' + data[i].Item + '</td>';
+   dat += '<td>' + data[i].Category + '</td>';
+   dat += '<td>' + data[i].Stock + '</td>';
+   dat += '<td>' + data[i].Price + '</td>';
+   dat += '</tr>';
+    }
+   
+  }
+  document.getElementById('sr').innerHTML = dat;
+  document.getElementById('s1').innerHTML = tf.rows[0].cells[0].innerHTML;
+  document.getElementById('s2').innerHTML = tf.rows[0].cells[1].innerHTML;
+  document.getElementById('s3').innerHTML = tf.rows[0].cells[2].innerHTML;
+  document.getElementById('s4').innerHTML = tf.rows[0].cells[3].innerHTML;
+  document.getElementById('s5').innerHTML = tf.rows[0].cells[4].innerHTML;
+  
 };
 
 request.send();
 }
+
+document.getElementById('srch').onclick = func;
+
 /*var myRequest = new XMLHttpRequest();
 myRequest.open('GET', 'http://localhost:3000/', true);
 myRequest.onload = function(){	
