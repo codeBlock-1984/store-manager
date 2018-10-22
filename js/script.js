aedc72a0e3d45dd87904e502f1faf91d40baba5f
IceOny <emmanuelroic@gmail.com>
2018-10-22 03:34:48 +0100

    add form for product

--- js/script.js
+++ js/script.js
@@ -36,39 +36,56 @@ function getData() {
 getData();
 }
 */
-/*function convertResponse(arr){
-  var i;
-  var resArr = [][];
-  var arrlen = arr.length;
 
-  for(i = 0; i < 5; i++){
-      resArr[i][0] = arr[i].ID;
+var modal = document.getElementById('myModal');
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var addBtn = document.getElementById("addB");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+addBtn.onclick = function(){
+  var pId = document.getElementById("id").value;
+  var pItem = document.getElementById("item").value;
+  var pCategory = document.getElementById("category").value;
+  var pStock = document.getElementById("stock").value;
+  var pPrice = document.getElementById("price").value;
+  var newProduct = {};
+
+  var addProdRequest = new XMLHttpRequest();
+  addProdRequest.open('POST', 'http://localhost:3000/api/v1/products');
+  addProdRequest.onload = function(){
+    newProduct.ID = pId;
+    newProduct.Item = pItem;
+    newProduct.Category = pCategory;
+    newProduct.Stock = pStock;
+    newProduct.Price = pPrice; 
   }
-  for(i = 0; i < 5; i++){
-      resArr[i][1] = arr[i].Item;
+  addProdRequest.send(JSON.parse(newProduct));
 
-  }
-  for(i = 0; i < 5; i++){
-      resArr[i][2] = arr[i].Category;
+}
 
-  }
-  for(i = 0; i < 5; i++){
-      resArr[i][3] = arr[i].Stock;
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
-  }
-  for(i = 0; i < 5; i++){
-      resArr[i][4] = arr[i].Price;
-
-  }
-  return resArr;
-}*/
 var func;
 func =  function () {
 var request = new XMLHttpRequest();
-request.open('GET', 'https://store-manager-app-api.herokuapp.com/api/v1/products');
-//request.open('GET', 'https://calm-beyond-30921.herokuapp.com');
-//request.responseType = 'text';
+request.open('GET', 'http://localhost:3000/api/v1/products');
 
 request.onload = function() {
   var data = JSON.parse(request.response);
