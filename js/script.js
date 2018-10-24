860c838335c53fd8b958f360fdbb65b3f35636af
IceOny <emmanuelroic@gmail.com>
2018-10-24 20:38:50 +0100

    restructured pages

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYe
 }
 document.getElementById('date').innerHTML = formatDate();
 
+/*
 function getProd(){
   document.getElementById('s1').innerHTML = tf.rows[0].cells[0].innerHTML;
   document.getElementById('s2').innerHTML = tf.rows[0].cells[1].innerHTML;
@@ -18,7 +19,9 @@ function getProd(){
   document.getElementById('s4').innerHTML = tf.rows[0].cells[3].innerHTML;
   document.getElementById('s5').innerHTML = tf.rows[0].cells[4].innerHTML;
 }
- document.getElementsByTagName("BODY").onload = getProd();
+ //document.getElementsByTagName("BODY").onload = getProd();
+
+
 /*var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -44,27 +47,78 @@ function getData() {
 getData();
 }
 */
+window.onload = function(){
+  var addToCart = document.getElementById("add-cart");
+addToCart.onclick = function(){
+  var cartTable = document.getElementById("cart-tab");
+  var newRow = cartTable.insertRow(-1);
+  var cell1 = row.insertCell(0).innerHTML;
+  var cell2 = row.insertCell(1).innerHTML;
+  var cell3 = row.insertCell(2).innerHTML;
+  var cell4 = row.insertCell(3).innerHTML;
+  var cell5 = row.insertCell(4).innerHTML;
+  var cell6 = row.insertCell(5).innerHTML;
+  var cell7 = row.insertCell(6).innerHTML;
+  var cell8 = row.insertCell(7).innerHTML;
+ 
+  cell1 = pId;
+  cell2 = pItem;
+  cell3 = pCategory;
+  cell4 = pStock;
+  cell5 = pPrice; 
+}
+}
 
-var modal = document.getElementById('myModal');
+
+
+
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var editBtn = document.getElementById("myBtn");
+var delBtn = document.getElementById("myBtn");
 var addBtn = document.getElementById("addB");
-
+var title = document.getElementById("mod-title");
+//var btnVal = document.getElementById("a");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+    title.innerHTML = "Add a new Product";
+    addBtn.innerHTML = "Add";
 }
-addBtn.onclick = function(){
+
+addBtn.onclick = function() {
+  //modal.style.display = "none";
   var pId = document.getElementById("id").value;
   var pItem = document.getElementById("item").value;
   var pCategory = document.getElementById("category").value;
   var pStock = document.getElementById("stock").value;
   var pPrice = document.getElementById("price").value;
-  var newProduct = {};
+  var prodTable = document.getElementById("sr");
+  var newRow = prodTable.insertRow(-1);
+  var cell1 = newRow.insertCell(0).innerHTML;
+  var cell2 = newRow.insertCell(1).innerHTML;
+  var cell3 = newRow.insertCell(2).innerHTML;
+  var cell4 = newRow.insertCell(3).innerHTML;
+  var cell5 = newRow.insertCell(4).innerHTML;
+  var cell6 = newRow.insertCell(5).innerHTML;
+  var cell7 = newRow.insertCell(6).innerHTML;
+  var cell8 = newRow.insertCell(7).innerHTML;
+ 
+  cell1 = pId;
+  cell2 = pItem;
+  cell3 = pCategory;
+  cell4 = pStock;
+  cell5 = pPrice;
+  cell6 = '<button id="" onclick="editProd"><img class="ic" src="img/edit.png"></button>';
+  cell7 = '<td><button onclick="delProd()"><img class="ic" src="img/delete.png"></button>';
+  
+
+  /*var newProduct = {};
 
   var addProdRequest = new XMLHttpRequest();
   addProdRequest.open('POST', 'http://localhost:3000/api/v1/products');
@@ -76,8 +130,9 @@ addBtn.onclick = function(){
     newProduct.Price = pPrice; 
   }
   addProdRequest.send(JSON.parse(newProduct));
-
+*/
 }
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
